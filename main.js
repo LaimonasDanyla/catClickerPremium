c465d169b2969b1b3fbf67de946dce99bb7090e7
Laimonas Danyla <LaimonasDanyla@users.noreply.github.com>
2017-10-27 09:29:11 +0200

    update photo links
    update photo links to show the image

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 var cats = {
   "catNames": [{
     "name": "Kitkat",
-    "url": "https://pixabay.com/static/uploads/photo/2012/10/12/17/12/cat-61079_960_720.jpg"
+    "url": "https://pixabay.com/photo-2083492/"
   }, {
     "name": "Kit-teacup",
-    "url":"https://pixabay.com/static/uploads/photo/2013/09/07/08/29/cat-179842_960_720.jpg"
+    "url":"https://pixabay.com/photo-2882170/"
   }, {
     "name": "Kittin around",
-    "url": "https://pixabay.com/static/uploads/photo/2013/12/12/03/08/kitten-227009_960_720.jpg"
+    "url": "https://pixabay.com/photo-636172/"
   }, {
     "name": "Kittles",
-    "url": "https://pixabay.com/static/uploads/photo/2015/11/15/20/21/cat-1044750_960_720.jpg"
+    "url": "https://pixabay.com/photo-67345/"
   }, {
     "name": "Kitty Kitty Bang Bang",
-    "url": "https://pixabay.com/static/uploads/photo/2016/05/18/21/33/cat-1401637_960_720.jpg"
+    "url": "https://pixabay.com/photo-697113/"
   }
 ]};
 
